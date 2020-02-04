d6019499d728467de8642880d9ad6743d3a78c67
Emmanuel Ndangurura <endangurura@gmail.com>
2020-02-04 16:39:27 +0300

    update tests

--- test-app/spec/features/swagger_ui_spec.rb
+++ test-app/spec/features/swagger_ui_spec.rb
@@ -7,6 +7,6 @@ RSpec.feature 'swagger-ui', js: true do
 
     expect(page).to have_content('blogs Searches blogs')
     expect(page).to have_content('blogs Creates a blog')
-    expect(page).to have_content('Retrieves a blog')
+    expect(page).to have_content('{id} Retrieves a blog')
   end
 end
